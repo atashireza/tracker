b586758b80d3f10b8922af698334cdd576c16477
atashireza <r.atashi2019@gmail.com>
2026-07-13 19:17:23 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-import streamlit as st
+import sys
+import streamlit.web.cli as stcli
 
-st.title("ParsPack Test")
-st.write("App is running successfully.")
+if __name__ == "__main__":
+    # اگر برنامه به صورت مستقیم با پایتون خام اجرا شد، آن را به سیستم اجرای استریم‌لیت هدایت می‌کند
+    sys.argv = [
+        "streamlit",
+        "run",
+        "app.py",
+        "--server.address=0.0.0.0",
+        "--server.port=8000",
+        "--server.headless=true"
+    ]
+    sys.exit(stcli.main())
